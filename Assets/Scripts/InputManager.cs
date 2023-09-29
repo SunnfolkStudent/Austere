8d5d6a19aceaebd9b842676fff9626cfbe91ad6a
Kris-Chil <Kristinemchilson@gmail.com>
2023-09-29 14:37:11 +0200

    Player Movement Scripts

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+// This variable is used to hold the Input value from WASD, Dpad or Left Stick
+    [HideInInspector] public Vector2 moveDirection;
+    
+    [Header("Interact")]
+    [HideInInspector] public bool canInteract;
+// These variables are used to hold Input Values from the F key or East Button
+    [HideInInspector] public bool interactPressed, interactReleased, interactHeld;
+
+// These variables are used to determine input source.
+    [SerializeField] private bool usingGamepad, usingDpad;
+
+// These variables are used to hold the current Input source
+    private Keyboard _keyboard;
+    private Gamepad _gamepad;
+
+    private void Start()
     {
-        
+    //Assign Input Sources to Variables
+        _keyboard = Keyboard.current;
+        _gamepad = Gamepad.current;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    // Check whether we are using Gamepad or Keyboard
+        if (usingGamepad && _gamepad != null)
+        {
+            UpdateGamepadInput();  
+        }
+        else
+        {
+            UpdateKeyboardInput();
+        }
     }
+    
+    private void UpdateKeyboardInput()
+    {
+    // Set the value of moveDirection to be equal to the value of wasd
+        moveDirection.x = (_keyboard.dKey.isPressed ? 1 : 0) + (_keyboard.aKey.isPressed ? -1 : 0);
+        moveDirection.y = (_keyboard.wKey.isPressed ? 1 : 0) + (_keyboard.sKey.isPressed ? -1 : 0);
+    // Set the interact bools when the f key is interacted with
+        interactPressed = _keyboard.fKey.wasPressedThisFrame;
+        interactReleased = _keyboard.fKey.wasReleasedThisFrame;
+        interactHeld = _keyboard.fKey.isPressed;
+    }
+    
+    private void UpdateGamepadInput()
+    {
+        if (usingDpad)
+        {
+            moveDirection.x = (_gamepad.dpad.right.isPressed ? 1 : 0) + (_gamepad.dpad.left.isPressed ? -1 : 0);
+            moveDirection.y = (_gamepad.dpad.up.isPressed ? 1 : 0) + (_gamepad.dpad.down.isPressed ? -1 : 0); 
+        }
+        else
+        {
+            moveDirection = _gamepad.leftStick.ReadValue();
+        }
+        
+        interactPressed = _gamepad.buttonEast.wasPressedThisFrame;
+        interactReleased = _gamepad.buttonEast.wasReleasedThisFrame;
+        interactHeld = _gamepad.buttonEast.isPressed;
+    }
+
+  
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    private Vector2 _desiredVelocity;
+    
+    [Header("Acceleration")]
+    public float accelerationTime = 0.02f;
+    public float groundFriction = 0.03f;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Components")]
+    private Rigidbody2D _rigidbody2D;
+    private InputManager _input;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _input = GetComponent<InputManager>();
+    }
+    
+    private void FixedUpdate()
     {
         
+        
+        if (_input.moveDirection.x != 0)
+        {
+            _desiredVelocity.x = Mathf.Lerp(_desiredVelocity.x, 
+                moveSpeed * _input.moveDirection.x, accelerationTime);
+        }
+        else
+        {
+            _desiredVelocity.x = Mathf.Lerp(_desiredVelocity.x, 0f, groundFriction);
+        }
+        
+        if (_input.moveDirection.y != 0)
+        {
+            _desiredVelocity.y = Mathf.Lerp(_desiredVelocity.y, 
+                moveSpeed * _input.moveDirection.y, accelerationTime);
+        }
+        else
+        {
+            _desiredVelocity.y = Mathf.Lerp(_desiredVelocity.y, 0f, groundFriction);
+        }
+        
+
+        _rigidbody2D.velocity = _desiredVelocity;
     }
 }
