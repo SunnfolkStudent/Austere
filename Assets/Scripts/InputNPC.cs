764651463f5dbb4f12885776cdbc34fe0ba54bc6
Kris-Chil <Kristinemchilson@gmail.com>
2023-10-02 12:02:51 +0200

    New Input scripts

---
+++ Assets/Scripts/InputNPC.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputNPC : MonoBehaviour
+{
+    public InputDialogue dialogue;
+
+    public void TriggerDialogue ()
+    {
+        FindFirstObjectByType<InputDialogueManager>().StartDialogue(dialogue);
+    }
+}
