a32979c0c7f84c2927875cacdac1b0738fa306c0
Elva03 <146328146+Elva03@users.noreply.github.com>
2023-10-03 18:25:39 +0200

    FIX????

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -14,13 +14,11 @@ public class PlayerMovement : MonoBehaviour
     [Header("Components")]
     private Rigidbody2D _rigidbody2D;
     private InputManager _input;
-    private Animator _anim;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _input = GetComponent<InputManager>();
-        _anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -44,15 +42,6 @@ public class PlayerMovement : MonoBehaviour
             {
                 _desiredVelocity.x = Mathf.Lerp(_desiredVelocity.x,
                     moveSpeed * _input.moveDirection.x, accelerationTime);
-                _anim.Play("Player_SideWalk");
-                if (_rigidbody2D.velocity.x < 0.01f)
-                {
-                    gameObject.transform.localScale = new Vector3(-1, 1, 1);
-                }
-                else
-                {
-                    gameObject.transform.localScale = new Vector3(1, 1, 1);
-                }
             }
             else
             {
@@ -63,23 +52,10 @@ public class PlayerMovement : MonoBehaviour
             {
                 _desiredVelocity.y = Mathf.Lerp(_desiredVelocity.y,
                     moveSpeed * _input.moveDirection.y, accelerationTime);
-                if (_rigidbody2D.velocity.y > 0.01f)
-                {
-                    _anim.Play("Player_UpWalk");
-                }
-                else
-                {
-                    _anim.Play("Player_DownWalk");
-                }
             }
             else
             {
                 _desiredVelocity.y = Mathf.Lerp(_desiredVelocity.y, 0f, groundFriction);
-            }
-
-            if (_input.moveDirection.x == 0f && _input.moveDirection.y == 0f)
-            {
-                _anim.Play("Player_Idle");
             }
 
             _rigidbody2D.velocity = _desiredVelocity;
