cdcc5adf52ae2f6a6c6e460e9e9b25ca9e98c70c
Kris-Chil <Kristinemchilson@gmail.com>
2023-10-03 18:22:44 +0200

    fix please

---
+++ Assets/Scripts/Player Scripts/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private InputManager _input;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _input = GetComponent<InputManager>();
+    }
+
+    private void Update()
+    {
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (_input.moveDirection.x == 0 && _input.moveDirection.y == 0)
+        {
+            _anim.SetBool("isWalking", false);
+        }else if (_input.moveDirection.x > 0)
+        {
+            _anim.SetBool("Walking");
+        }
+    }
+}
+*/
