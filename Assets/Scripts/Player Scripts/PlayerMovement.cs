cec1aa722b1c3ce6816378aed37580167dd693ce
Kris-Chil <Kristinemchilson@gmail.com>
2023-10-03 18:05:45 +0200

    player animation gone ;-;

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -14,13 +14,11 @@ public class PlayerMovement : MonoBehaviour
     [Header("Components")]
     private Rigidbody2D _rigidbody2D;
     private InputManager _input;
-    private Animator _anim;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _input = GetComponent<InputManager>();
-        _anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -44,10 +42,6 @@ public class PlayerMovement : MonoBehaviour
             {
                 _desiredVelocity.x = Mathf.Lerp(_desiredVelocity.x,
                     moveSpeed * _input.moveDirection.x, accelerationTime);
-                _anim.SetBool("isWalking", true);
-                _anim.SetBool("sideWalk", true);
-                _anim.SetBool("upWalk", false);
-                _anim.SetBool("downWalk", false);
             }
             else
             {
@@ -58,33 +52,13 @@ public class PlayerMovement : MonoBehaviour
             {
                 _desiredVelocity.y = Mathf.Lerp(_desiredVelocity.y,
                     moveSpeed * _input.moveDirection.y, accelerationTime);
-                _anim.SetBool("isWalking", true);
-                if (_rigidbody2D.velocity.y > 0.01f)
-                {
-                    _anim.SetBool("upWalk", true);
-                    _anim.SetBool("sideWalk", false);
-                }
-                else
-                {
-                    _anim.SetBool("downWalk", true);
-                    _anim.SetBool("sideWalk", false);
-                }
             }
             else
             {
                 _desiredVelocity.y = Mathf.Lerp(_desiredVelocity.y, 0f, groundFriction);
             }
 
-            if (_input.moveDirection.x == 0f && _input.moveDirection.y == 0f)
-            {
-                _anim.SetBool("isWalking", false);
-                _anim.SetBool("sideWalk", false);
-                _anim.SetBool("downWalk", false);
-                _anim.SetBool("upWalk", false);
-            }
-
             _rigidbody2D.velocity = _desiredVelocity;
-            transform.localScale = new Vector3(_input.moveDirection.x, 1, 1);
         }
     }
 }
