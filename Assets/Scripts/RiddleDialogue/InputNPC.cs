90ffbed4597b8d3e8d37f0d8e50d145c24809a0f
Kris-Chil <Kristinemchilson@gmail.com>
2023-10-03 10:26:27 +0200

    updated InputNPC script

--- Assets/Scripts/RiddleDialogue/InputNPC.cs
+++ Assets/Scripts/RiddleDialogue/InputNPC.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,21 @@ using UnityEngine;
 public class InputNPC : MonoBehaviour
 {
     public Dialogue dialogue;
+
+    private InputManager _input;
+
+    private void Start()
+    {
+        _input = GetComponent<InputManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && _input.interactHeld)
+        {
+            TriggerDialogue();
+        }
+    }
 
     public void TriggerDialogue ()
     {
