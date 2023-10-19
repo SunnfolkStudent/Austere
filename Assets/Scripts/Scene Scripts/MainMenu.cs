585e111af415775427b962f1e464ea6819fc692d
Kris-Chil <Kristinemchilson@gmail.com>
2023-10-19 14:55:06 +0200

    current set of menu scripts

--- Assets/Scripts/Scene Scripts/MainMenu.cs
+++ Assets/Scripts/Scene Scripts/MainMenu.cs
@@ -6,21 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private void Awake()
-    {
-        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().PlayMusic();
-    }
-
     public void PlayGame()
     {
-        //GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().StopMusic();
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().StopMusic();
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene("Intro_Scene");
     }
 
     public void Settings()
     {
-        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().PlayMusic();
         SceneManager.LoadScene("Controls");
     }
 
